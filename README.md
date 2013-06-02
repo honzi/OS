a3379417a65086e82d1acdadc6ddd2ab222cbf43
Jan Horava <czechinthemail@gmail.com>
2013-06-02 03:38:01 -0700

    added project files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-OS
-==
+Beginnings of a custom OS, written in assembly.
+
+Currently written in NASM assembly for x86 PCs, based off code from http://mikeos.berlios.de/write-your-own-os.html.
---
+++ kernel.asm
@@ -0,0 +1,32 @@
+BITS 16
+;------------------------------------------;
+mov ax,07C0h
+add ax,288
+mov ss,ax
+mov sp,4096
+
+mov ax,07C0h
+mov ds,ax
+
+mov si, text_string
+call print_string
+
+jmp $
+;------------------------------------------;
+text_string db 'OS',0
+;------------------------------------------;
+print_string:
+    mov ah, 0Eh
+
+.repeat:              ;print each character
+    lodsb
+    cmp al,0
+    je .done          ;until last char is 0
+    int 10h
+    jmp .repeat
+
+.done:
+    ret
+;------------------------------------------;
+times 510-($-$$) db 0
+dw 0xAA55
