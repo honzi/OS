dea23c9e88da3f6a65c4af78dfd1dfda297310af
Jan Horava <czechinthemail@gmail.com>
2013-06-04 15:50:50 -0700

    improved formatting

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -8,7 +8,7 @@ mov sp,4096
 mov ax,07C0h
 mov ds,ax
 
-mov si, text_string
+mov si,text_string
 call print_string
 
 jmp $
@@ -16,17 +16,14 @@ jmp $
 text_string db 'OS',0
 ;------------------------------------------;
 print_string:
-    mov ah, 0Eh
+    mov ah,0Eh
 
 .repeat:              ;print each character
     lodsb
     cmp al,0
-    je .done          ;until last char is 0
+    je ret            ;until last char is 0
     int 10h
     jmp .repeat
-
-.done:
-    ret
 ;------------------------------------------;
 times 510-($-$$) db 0
 dw 0xAA55
