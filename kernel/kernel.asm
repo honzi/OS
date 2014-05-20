a9fd5457e702b85a4f72a613a3c2d9334dc779db
Jan Horava <czechinthemail@gmail.com>
2014-05-19 21:50:14 -0700

    added os_print_newline function

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -14,15 +14,24 @@ mov ds,ax
 
   ;print strings
 mov si,string_os_info
-call os_print_string
+  call os_print_string
+call os_print_newline
 mov si,string_os_reboot
-call os_print_string
+  call os_print_string
 
   ;wait for keyboard input before reboot
 mov ah,00h
 int 16h
 call os_reboot
 ;----------------------;
+os_print_newline:
+    mov ah,0eh ;tty mode write characters
+    mov al,13
+    int 10h
+    mov al,10
+    int 10h
+    ret
+
 os_print_string:
     mov ah,0eh ;tty mode write characters
 
@@ -39,8 +48,8 @@ os_reboot:
 os_ret:
     ret
 ;----------------------;
-string_os_info db 'http://iterami.com OS v2014.05.19',10,13,0
-string_os_reboot db 'Press any key to reboot.',10,13,0
+string_os_info db 'http://iterami.com OS v2014.05.19',0
+string_os_reboot db 'Press any key to reboot.',0
 ;----------------------;
 times 510-($-$$) db 0
 dw 0xaa55
