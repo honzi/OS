32ebad55891e0dc57f3891b37eed6a1049116354
Jan Horava <czechinthemail@gmail.com>
2014-05-20 19:19:18 -0700

    added pusha popa to string fuctions, removed os_ret function, improved formatting

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -25,30 +25,36 @@ int 16h
 call os_reboot
 ;----------------------;
 os_print_newline:
+    pusha
     mov ah,0eh ;tty mode write characters
+
     mov al,13
     int 10h
     mov al,10
     int 10h
+
+    popa
     ret
 
 os_print_string:
+    pusha
     mov ah,0eh ;tty mode write characters
 
     .loop:
         lodsb
         cmp al,0
-          je os_ret
+          je .done
         int 10h
         jmp .loop
+    
+    .done:
+        popa
+        ret
 
 os_reboot:
     int 19h
-
-os_ret:
-    ret
 ;----------------------;
-string_os_info db 'http://iterami.com OS v2014.05.19',0
+string_os_info db 'http://iterami.com OS v2014.05.20',0
 string_os_reboot db 'Press any key to reboot.',0
 ;----------------------;
 times 510-($-$$) db 0
