7027072a15432f8f1985624c66845b03cbb72552
Jan Horava <czechinthemail@gmail.com>
2015-06-18 20:47:55 -0700

    standardized version string

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -90,7 +90,7 @@ os_reboot:
 ;----------------------;
 string_os_cli_invalid db 'Invalid command.',0
 string_os_cli_prompt db '>',0
-string_os_info db 'http://iterami.com OS v2014.05.24',0
+string_os_info db 'iterami/OS v20150618.0',0
 ;----------------------;
 times 510-($-$$) db 0
 dw 0xaa55
